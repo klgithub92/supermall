8c016fcba91880efdbbf9c9ff380fda81cb07ac1
klgit92 <1538374862@qq.com>
2021-06-17 23:54:28 +0800

    第二天上午

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,54 @@
+// 首页tab栏切换封装
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item" v-for="(item, index) in titles" :key="index" :class="{ active: index === currentIndex }" @click="tabClick(index)">
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      titles: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0
+      }
+    },
+    methods: {
+      tabClick(index) {
+        this.currentIndex = index
+      }
+    }
+  }
+</script>
+
+<style>
+.tab-control {
+  display: flex;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  font-size: 15px;
+  background-color: #fff;
+}
+.tab-control-item {
+  flex: 1;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+.active {
+  color: var(--color-high-text);
+}
+.active span {
+  border-bottom: 2px solid var(--color-high-text);
+}
+</style>
